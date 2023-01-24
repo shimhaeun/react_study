57e3530bff69c5be36f707cae9bf04a750579254
haeun <shy624@naver.com>
2023-01-24 17:43:24 +0900

    컴포넌트화

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class Subject extends Component {
   render() {
     return(
-    <div>
-    Hello, React!!!
-    </div>
+    <header>
+      <h1>WEB</h1>
+      World Wide Web!
+    </header>
     );
   }
 }
 
+class TOC extends Component {
+  render () {
+    return (
+      <nav>
+        <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+            HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
+      </div>
+    );
+  }
+}
 export default App;
