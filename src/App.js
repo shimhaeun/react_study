ba02db1d683c0f54a36551594a81df59981eedd3
haeun <shy624@naver.com>
2023-01-24 20:35:52 +0900

    컴포넌트를 파일로 분리

--- src/App.js
+++ src/App.js
@@ -1,48 +1,17 @@
 import React, { Component } from 'react';
+import TOC from './components/TOC';
+import Subject from './components/Subject';
+import Content from './components/Content'
 import './App.css';
 
-class Subject extends Component {
-  render() {
-    return(
-    <header>
-      <h1>WEB</h1>
-      World Wide Web!
-    </header>
-    );
-  }
-}
-
-class TOC extends Component {
-  render () {
-    return (
-      <nav>
-        <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="2.html">CSS</a></li>
-            <li><a href="3.html">JavaScript</a></li>
-        </ul>
-      </nav>
-    )
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>HTML</h2>
-            HTML is HyperText Markup Language.
-      </article>
-    );
-  }
-}
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Subject></Subject>
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        {/* <Subject title="React" sub="For UI"></Subject> */}
         <TOC></TOC>
-        <Content></Content>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
