c40c7134acbdc902ac81de155f620d3b34046caf
haeun <shy624@naver.com>
2023-01-25 01:22:53 +0900

    rendering check

--- src/App.js
+++ src/App.js
@@ -5,15 +5,39 @@ import Content from './components/Content'
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'read',
+      mode: 'welcome',
+      subject: {title:'WEB', sub:"world wide web"},
+      welcome: {title:'Welcome', desc: "hello, React!"},
+      contents: [
+        {id:1, title: 'HTML', desc:'HTML is for information'},
+        {id:2, title: 'CSS', desc:'CSS is for design'},
+        {id:3, title: 'JavaScript', desc: 'JavaScript is for interactive'}
+      ]
+    }
+  }
+   render() {
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className='App'>
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        {/* <Subject title="React" sub="For UI"></Subject> */}
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Subject
+        title={this.state.subject.title}
+        sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
-  }
+}
 }
 export default App;
